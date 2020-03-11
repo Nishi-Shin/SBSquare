5c40d84236e6807e0020958e47cac87c85353474
Nishi-Shin <se031247@gmail.com>
2020-03-11 23:36:01 +0900

    new_commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root  'tweets#index'
   get 'tweets' => 'tweets#index'
+  get 'tweets/new' => 'tweets#new'
 end
