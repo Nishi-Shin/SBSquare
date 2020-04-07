401f397e4f2f95692cad5262f33de3209007a5d0
Nishi-Shin <se031247@gmail.com>
2020-04-07 23:47:17 +0900

    page_commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+  get '/home', to:'pages#home'
   devise_for :users
   root 'tweets#index'
-  resources :tweets do
+    resources :tweets do
     resources :comments, only: [:create]
   end
   resources :tweets                     #tweets_controllerに対してのresourcesメソッド
